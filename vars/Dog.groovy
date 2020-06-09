2bf1fa9de2b80737aaf4836af0997728c6273841
yafeng.xing <yafeng.xing@tcl.com>
2020-06-09 14:04:02 +0800

    test class

---
+++ vars/Dog.groovy
@@ -0,0 +1,34 @@
+class Dog {
+
+    def name;
+
+    Dog() {
+
+    }
+
+    Dog(name) {
+        this.name = name
+    }
+
+    def getName() {
+        return name
+    }
+
+    void setName(name) {
+        this.name = name
+    }
+
+    def run() {
+
+        println(this.name + " is running ...")
+    }
+
+    public static void main(String[] args) {
+
+        Dog d = new Dog()
+        d.setName("dahuang")
+        println(d.getName())
+
+        d.run()
+    }
+}
