fd99aa15bce1d9cf43029cfc904cc4603fc73d3e
yafeng.xing <yafeng.xing@tcl.com>
2020-06-09 15:48:56 +0800

    test run func

--- src/org/devops/Dog.groovy
+++ src/org/devops/Dog.groovy
@@ -22,18 +22,11 @@ class Dog {
     }
 
     def run() {
-        PrintMes("测试run方法","green")
         setName("小小鸟")
-    }
 
-    def PrintMes(value,clr){
-        def colors = ['red'   : "\033[40;31m >>>>>>>>>>>${value}<<<<<<<<<<< \033[0m",
-                  'blue'  : "\033[47;34m ${value} \033[0m",
-                  'green' : "[1;32m>>>>>>>>>>${value}>>>>>>>>>>[m",
-                  'green1' : "\033[40;32m >>>>>>>>>>>${value}<<<<<<<<<<< \033[0m" ]
-        ansiColor('xterm') {
-            println(colors[clr])
-        }
+        sh """
+            echo $name
+        """
     }
 
     static void main(String[] args) {
