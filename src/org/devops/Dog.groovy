aa6d58c1a4016b831446c74db3a018c8bb460d3a
yafeng.xing <yafeng.xing@tcl.com>
2020-06-09 15:41:43 +0800

    test run, add print mes

--- src/org/devops/Dog.groovy
+++ src/org/devops/Dog.groovy
@@ -26,13 +26,13 @@ class Dog {
         setName("小小鸟")
     }
 
-    def PrintMes(value,color){
-        colors = ['red'   : "\033[40;31m >>>>>>>>>>>${value}<<<<<<<<<<< \033[0m",
+    def PrintMes(value,clr){
+        def colors = ['red'   : "\033[40;31m >>>>>>>>>>>${value}<<<<<<<<<<< \033[0m",
                   'blue'  : "\033[47;34m ${value} \033[0m",
                   'green' : "[1;32m>>>>>>>>>>${value}>>>>>>>>>>[m",
                   'green1' : "\033[40;32m >>>>>>>>>>>${value}<<<<<<<<<<< \033[0m" ]
         ansiColor('xterm') {
-            println(colors[color])
+            println(colors[clr])
         }
     }
 
