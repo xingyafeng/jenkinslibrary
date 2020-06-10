a37d6b3421a8f32348b41b15c07e27b6dc350175
yafeng.xing <yafeng.xing@tcl.com>
2020-06-10 15:40:40 +0800

    test class

---
+++ src/org/devops/Cat.groovy
@@ -0,0 +1,25 @@
+package org.devops
+
+class Cat {
+
+    def name;
+
+    Cat(name) {
+        this.name = name
+    }
+
+    def getName() {
+        return name
+    }
+
+    void setName(name) {
+        this.name = name
+    }
+
+    void run() {
+        def ret = this.getClass().getSimpleName() + " : " + name + " is runing ..."
+        def tools = new tools()
+
+        tools.i("$ret")
+    }
+}
